efa37255d0f2f91af9597768bf8d070ce66dd30b
Swwapnil1182 <swapniljadhav1182@gmail.com>
2025-05-17 11:33:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function insert_Row() {
     //Write your code here
+
+	const sampleTable=document.getElementById("sampleTable")
+	const row=document.createElement('tr')
+
+	row.innerHTML=
+		`
+		<td>New Cell1</td>
+		<td>New Cell2</td>
+		`
+
+	
+	sampleTable.insertAdjacentElement('afterbegin',row)
   
   
 }
